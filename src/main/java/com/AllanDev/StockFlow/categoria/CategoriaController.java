cdda104e1109c021569c503aff945b3eaea6318d
DevAllanEwerton <allanewerton24102004@gmail.com>
2026-08-28 00:33:57 -0300

    Feat: cria as rotas de caregoria

--- src/main/java/com/AllanDev/StockFlow/categoria/CategoriaController.java
+++ src/main/java/com/AllanDev/StockFlow/categoria/CategoriaController.java
@@ -1,4 +1,43 @@
 package com.AllanDev.StockFlow.categoria;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Categoria")
 public class CategoriaController {
+    @GetMapping
+    public String test() {
+        return "Ola mundo";
+    }
+
+    //    add Categoria
+    @PostMapping("/criar")
+    public String criarCategoria() {
+        return "criarCategoria";
+    }
+
+    //    mostra Categoria por ID
+    @GetMapping("/ID")
+    public String procuraCategoriaID() {
+        return "procuraCategoriaID";
+    }
+
+    //    Mostra todos os Categoria
+    @GetMapping("/lista")
+    public String listarCategoria() {
+        return "listarCategoria";
+    }
+
+    //    Alterar dados dos Categoria
+    @PutMapping("/alterarid")
+    public String alterarCategoria() {
+        return "alterarCategoria";
+    }
+
+    //    Deletar Categoria
+    @DeleteMapping("/detarID")
+    public String deletarCategoria() {
+        return "deletarCategoria";
+    }
+
 }
--- src/main/java/com/AllanDev/StockFlow/produto/ProdutoController.java
+++ src/main/java/com/AllanDev/StockFlow/produto/ProdutoController.java
@@ -1,14 +1,43 @@
 package com.AllanDev.StockFlow.produto;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class ProdutoController {
     @GetMapping
-    public String test(){
+    public String test() {
         return "Ola mundo";
     }
+
+    //    add produto
+    @PostMapping("/criar")
+    public String criarProduto() {
+        return "criarProduto";
+    }
+
+    //    mostra produto por ID
+    @GetMapping("/produtoID")
+    public String procuraProdutoID() {
+        return "procuraProdutoID";
+    }
+
+    //    Mostra todos os produtos
+    @GetMapping("/produtos")
+    public String listarProduto() {
+        return "listarProduto";
+    }
+
+    //    Alterar dados dos produtos
+    @PutMapping("/alterarID")
+    public String alterarProduto() {
+        return "alterarProduto";
+    }
+
+    //    Deletar produto
+    @DeleteMapping("/detarID")
+    public String deletarProduto() {
+        return "deletarProduto";
+    }
+
 }
